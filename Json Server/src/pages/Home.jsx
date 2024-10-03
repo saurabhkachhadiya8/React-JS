4d2760e378819ae5031cfde6fc917c6b8b71ee6d
saurabhkachhadiya8 <saurabhkachhadiya890@gmail.com>
2024-10-03 12:32:16 +0530

    React JS

--- Json Server/src/pages/Home.jsx
+++ Json Server/src/pages/Home.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      let data = await fetch(`http://localhost:8000/users`);
+      let res = await data.json();
+      setUserData(res);
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div align="center">
+      <h1>Json Server</h1>
+      <table border={1} width={'30%'} cellPadding={10}>
+        <thead>
+          <tr>
+            <th width={'20%'}>Sr No</th>
+            <th>Name</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userData.map((user, i) => {
+              const { id, name, phone } = user;
+              return (
+                <tr key={++i}>
+                  <td align='center'>{++i}</td>
+                  <td>{name}</td>
+                  <td>{phone}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
