8d17a28cf2834c90bfb1ca9b50eed5d1a4f3551c
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-10-03 16:33:38 +0530

    React JS

---
+++ Json-Server CRUD/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Add from './crud/Add'
+
+const Home = () => {
+  return (
+    <div align="center">
+        <h1>Json Server Crud</h1>
+        <Add/>
+    </div>
+  )
+}
+
+export default Home
