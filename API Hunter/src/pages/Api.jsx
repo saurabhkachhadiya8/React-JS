74a6b59ea7009cb353df78cb439719702af9ea2e
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-10-17 16:59:50 +0530

    React JS

--- API Hunter/src/pages/Api.jsx
+++ API Hunter/src/pages/Api.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react';
 
-const api = () => {
+const Api = () => {
+  const [data, setData] = useState([]);
 
-    // const fetchData = () => {
-    //     fetch('https://dummyjson.com/quotes')
-    //     .then(res => res.json)
-    //     .then(data => {console.log(data.quotes)});
-    // }
-    // useEffect(() => {
-    //     fetchData();
-    // },[])
+  const fetchData = () => {
+    fetch(`https://dummyjson.com/products`)
+      .then(res => res.json())
+      .then(AllPost => {
+        setData(AllPost.products);
+      })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
     <div align="center">
-        <h1>API Using Fetch Method</h1>
+      <h1>API Using Fetch Method</h1>
+      <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center'}}>
+      {
+        data.map((p) => {
+          const {id,title,description,category,price,thumbnail} = p;
+          return(
+            <div key={id} style={{border:'1px solid #000',padding:'10px'}}>
+              <div className="card">
+                <img src={thumbnail} alt={thumbnail} width={'200'}/>
+              </div>
+            </div>
+          )
+        })
+      }
+      </div>
     </div>
   )
 }
 
-export default api
+export default Api
